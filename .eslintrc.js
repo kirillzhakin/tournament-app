9f877f4e323ec93b2f8b6c8c34055f0ce08795b3
Кирилл Жакин <kirillzhakin@Mac-mini-Kirill.local>
2024-06-17 08:37:12 +0500

    fix: config

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+  },
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+    ecmaVersion: 2020,
+    sourceType: "module",
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+        singleQuote: false,
+        tabWidth: 2,
+        useTabs: false,
+      },
+    ],
+  },
+};
